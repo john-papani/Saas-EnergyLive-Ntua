bff92a478626059eaf256bd3960d3a4d15abc462
john-papani <johnpapani1@gmail.com>
2022-05-17 13:08:17 +0300

    2022-05-17 frontendUpdate

--- frontend/src/Pages/DexiaMain.js
+++ frontend/src/Pages/DexiaMain.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/main.css";
 import { Button } from "react-bootstrap";
@@ -7,75 +7,7 @@ import axios from "axios";
 import Chart from "chart.js/auto";
 
 const DexiaMain = ({ quantity, country, type, startDate }) => {
-  return (
-    <div>
-      <div class="container">
-        <div class="col">
-          <div
-            className={`${quantity === "Generation Per Type" ? "" : "d-none"}`}
-          >
-            <div class="row">
-              <div class="col-sm-4"> {quantity} </div>{" "}
-              <div class="col-sm-4" id="type">
-                {" "}
-                {type}{" "}
-              </div>{" "}
-              <div class="col-sm-4" id="country">
-                {" "}
-                {country}{" "}
-              </div>{" "}
-            </div>{" "}
-          </div>{" "}
-          <div
-            className={`${quantity === "Actual Total Load" ? "" : "d-none"}`}
-          >
-            <div class="row">
-              <div class="col-7"> {quantity} </div>{" "}
-              <div class="col-5"> {country} </div>{" "}
-            </div>{" "}
-          </div>{" "}
-          {/*! ----------------------------------------------------- */}
-          {/* <div> */}
-          {/* {data.map((item) => (
-               {item}
-            ))}
-          </div> */}
-          {/*! ----------------------------------------------------- */}
-          <div class="row">
-            <DexiaChart />
-          </div>{" "}
-          <div class="row">
-            <p> Last Update time: </p>{" "}
-          </div>{" "}
-          <div class="row">
-            <div class="col-9">
-              <Button> Download Image </Button>{" "}
-            </div>{" "}
-            <div class="col-3">
-              <Button> Download data </Button>{" "}
-            </div>{" "}
-          </div>{" "}
-          <div class="footer-dexia">
-            <hr class="solid" />
-            <div class="row">
-              <div class="col-sm-3"> Service Status: </div>{" "}
-              <div class="col-sm-3"> Days Left: </div>{" "}
-              <div class="col-sm-3">
-                <a href="/extend"> Extend Plan </a>
-              </div>{" "}
-              <div class="col-sm-3">
-                <a href="/about"> About </a>{" "}
-              </div>{" "}
-            </div>{" "}
-          </div>{" "}
-        </div>{" "}
-      </div>{" "}
-    </div>
-  );
-};
-
-export default DexiaMain;
-const DexiaChart = () => {
+  const ref = useRef(null);
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -87,35 +19,106 @@ const DexiaChart = () => {
     getCountryData();
   }, []);
 
-  return (
-    <Line
-      data={{
-        labels: data?.map((data) => data.id),
+  const downloadImage = useCallback(() => {
+    const link = document.createElement("a");
+    link.download = "chart.jpg";
+    link.href = ref.current.toBase64Image();
+    link.click();
+  }, []);
 
-        datasets: [
-          {
-            borderColor: "blue",
-            backgroundColor: "black",
-            label: "Cases",
-            data: data?.map((data) => data.userId),
-            tension: 0.1,
-          },
-        ],
-      }}
-      options={{
-        plugins: {
-          title: {
-            display: false,
-            text: "",
-          },
-          legend: {
-            display: true,
-            position: "bottom",
-          },
-        },
-      }}
-      width={100}
-      height={40}
-    />
+  return (
+    <div>
+      <div class="container">
+        <div class="col">
+          <div
+            className={`${quantity === "Generation Per Type" ? "" : "d-none"}`}
+          >
+            <div class="row">
+              <div class="col-sm-4"> {quantity} </div>
+              <div class="col-sm-4" id="type">
+                {type}
+              </div>
+              <div class="col-sm-4" id="country">
+                {country}
+              </div>
+            </div>
+          </div>
+          <div
+            className={`${quantity === "Actual Total Load" ? "" : "d-none"}`}
+          >
+            <div class="row">
+              <div class="col-7"> {quantity} </div>
+              <div class="col-5"> {country} </div>
+            </div>
+          </div>
+          {/*! ----------------------------------------------------- */}
+          {/* <div> */}
+          {/* {data.map((item) => (
+               {item}
+            ))}
+          </div> */}
+          {/*! ----------------------------------------------------- */}
+          <div class="row">
+            {/* <DexiaChart ref={ref} /> */}
+            <Line
+              ref={ref}
+              data={{
+                labels: data?.map((data) => data.id),
+
+                datasets: [
+                  {
+                    borderColor: "blue",
+                    backgroundColor: "black",
+                    label: "Cases",
+                    data: data?.map((data) => data.userId),
+                    tension: 0.1,
+                  },
+                ],
+              }}
+              options={{
+                plugins: {
+                  title: {
+                    display: false,
+                    text: "",
+                  },
+                  legend: {
+                    display: true,
+                    position: "bottom",
+                  },
+                },
+              }}
+              width={100}
+              height={40}
+            />
+          </div>
+          <div class="row">
+            <p> Last Update time: </p>
+          </div>
+          <div class="row">
+            <div class="col-9">
+              <Button onClick={() => downloadImage()}> Download Image </Button>
+            </div>
+            <div class="col-3">
+              <Button> Download data </Button>
+            </div>
+          </div>
+          <div class="footer-dexia">
+            <hr class="solid" />
+            <div class="row">
+              <div class="col-sm-3"> Service Status: </div>
+              <div class="col-sm-3"> Days Left: </div>
+              <div class="col-sm-3">
+                <a href="/extend"> Extend Plan </a>
+              </div>
+              <div class="col-sm-3">
+                <a href="/about"> About </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
+
+export default DexiaMain;
