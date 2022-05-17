1d4cab4288dcd675841e5ef196f708d8f9805c70
Eleftherios Tsonis <94307935+el18004@users.noreply.github.com>
2022-05-17 16:02:08 +0300

    Route and Controller for ATL microservice

---
+++ actual-total-load/controllers/atlController.js
@@ -0,0 +1,29 @@
+const atl = require("../models/atl");
+const mongoose = require('mongoose');
+
+exports.get = async function (req, res) {
+    try{
+        await mongoose.connect('mongodb://localhost:27017/EnergyLive');
+        console.log("Connected to DB");
+        // get date_from
+        var dateFrom = req.params['date_from'];
+        //dateFrom = dateFrom + 'T00:00:00.00.000Z';
+        console.log(dateFrom)
+        // get country
+        const country = req.params['country'];
+        console.log(country)
+        // db query
+        const ATL = await atl.find({ AreaTypeCode : "CTY", MapCode : country, DateTime : {$gte: dateFrom}}).sort('DateTime'); 
+        console.log(ATL)
+        var array = [];
+        for(let i = 0; i < ATL.length; i++) {
+            var ATLTimeStamp = ATL[i].DateTime;
+            var loadValue = ATL[i].TotalLoadValue;
+            var row = { 'ATLTimeStamp' : ATLTimeStamp, 'loadValue' : loadValue };
+            array.push(row);
+        }
+        res.status(200).send(array);
+    } catch (error) {
+        res.status(500).send("Internal Server Error")
+    }
+}
