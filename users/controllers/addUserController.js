f93c937ad0f645ab5f4d8fbcf9758b572b4b54c5
Eleftherios Tsonis <94307935+el18004@users.noreply.github.com>
2022-05-23 14:10:36 +0300

    First 2 endpoints for users ms
    end points useful for finding users in the database or adding new ones

---
+++ users/controllers/addUserController.js
@@ -0,0 +1,31 @@
+const users = require("../models/users");
+const mongoose = require('mongoose');
+
+exports.post = async function (req, res) {
+    try{
+        await mongoose.connect('mongodb://localhost:27017/EnergyLiveUsers');
+
+        var first = req.params['first_name'];
+        var last = req.params['last_name'];
+        var addr = req.params['email'];
+
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+        today.setDate(today.getDate() + 30);
+             
+        // create document
+        const newuser = new users({ first_name : first, last_name: last, email: addr, valid_until : today });
+        console.log(today)
+        // save model to database
+        newuser.save(function (err, usr) {
+            console.log('hello')
+            if (err) return console.error('err');
+            console.log(newuser.first_name + " saved to collection.");
+        });
+
+    } catch (error) {
+        res.status(500).send("Internal Server Error")
+    }
+}
---
+++ users/controllers/findUserController.js
@@ -0,0 +1,18 @@
+const users = require("../models/users");
+const mongoose = require('mongoose');
+
+exports.get = async function (req, res) {
+    try{
+        await mongoose.connect('mongodb://localhost:27017/EnergyLiveUsers');
+        // get date_from
+        var addr = req.params['email'];
+        console.log(addr)
+        // db query
+        const user = await users.find({ email : addr }); 
+        console.log("after")
+        var ans = { 'first_name' : user[0].first_name, 'last_name' : user[0].last_name, 'email' : user[0].email, 'valid_until' : user[0].valid_until };
+        res.status(200).send(ans);
+    } catch (error) {
+        res.status(500).send("Internal Server Error")
+    }
+}
